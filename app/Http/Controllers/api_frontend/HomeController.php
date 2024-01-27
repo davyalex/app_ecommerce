e29248c548e8ba10ce2b203296bb487e52bcc620
davyalex <alexkouamelan96pv@gmail.com>
2024-01-27 18:58:41 +0000

    optimize

--- app/Http/Controllers/api_frontend/HomeController.php
+++ app/Http/Controllers/api_frontend/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     {
         try {
             $data = Category::with([
-               'products'=> fn ($q) =>[ $q->with('media'), $q->skip(0)->take(15)]
+               'products'=> fn ($q) =>[ $q->with('media'), $q->paginate(15)]
 
 
                ,'media', 'subcategories' => fn ($q) => $q->with('media')
