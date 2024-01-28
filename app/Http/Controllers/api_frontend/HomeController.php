18388de8b77475d07dd79e03ec399f2d7f180363
davyalex <alexkouamelan96pv@gmail.com>
2024-01-28 20:51:08 +0000

    optimize

--- app/Http/Controllers/api_frontend/HomeController.php
+++ app/Http/Controllers/api_frontend/HomeController.php
@@ -135,7 +135,11 @@ class HomeController extends Controller
     /**
      * @OA\Get(
      *     path="/api/v1/allProduct",
-     *     summary="Liste de tous les produits",
+     *     summary="Liste de tous les produits:
+     *     Pour  recuperer un produit par sa categorie envoyez 
+     *     l'id de la catégorie dans le paramètre en GET {category} 
+     *     ou {subcategory} pour recuperer les produits d'une sous categorie 
+     * Exemple: http://127.0.0.1:8000/api/v1/allProduct?category={id}",
      *     tags={"Liste de tous les produits "},
      *     @OA\Response(response=200, description="Successful operation"),
      * )
@@ -162,7 +166,6 @@ class HomeController extends Controller
                 "description" => 'Liste des produits || parametre: category_id or subcategory_id',
 
             ], 200);
-
         } catch (\Exception $e) {
             $e->getMessage();
         }
