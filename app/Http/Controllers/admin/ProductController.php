9f8d30a3942d6c5475beae659fbb45d08dee14c9
davyalex <alexkouamelan96pv@gmail.com>
2024-02-29 09:52:48 +0000

    optimize

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -37,6 +37,10 @@ class ProductController extends Controller
                 ->when($type, fn ($q) => $q->whereType($type))
                 ->orderBy('created_at', 'DESC')
                 ->get();
+
+            // dd($product->toArray());
+            return view('admin.pages.product.index', compact('product'));
+            
         } elseif ($auth = Auth::check()  && $auth_role == 'administrateur') {
             //recuperation des produits en fonction du filtre
             $product = Product::with(['categories', 'subcategorie', 'media', 'user'])
@@ -44,11 +48,13 @@ class ProductController extends Controller
                 ->when($type, fn ($q) => $q->whereType($type))
                 ->orderBy('created_at', 'DESC')
                 ->get();
+
+
+            // dd($product->toArray());
+            return view('admin.pages.product.index', compact('product'));
         }
 
 
-        // dd($product->toArray());
-        return view('admin.pages.product.index', compact('product'));
     }
 
     /**
